ccb99c37242e4bebc04cbf9f36f3b60db8f8c18e
Shay Peleg <shaypeleg1@gmail.com>
2017-01-02 14:47:41 +0200

    add plane

--- src/components/plane/plane-add.vue
+++ src/components/plane/plane-add.vue
@@ -3,10 +3,20 @@
 
   <section class="plane-add">
     <h2>Add New Plane</h2>
-    <div class="row">
-     <p>New Plane</p>
+    <div class="new-plane-form">
+     <label class="subtitle is-5">Plane Model</label>
+        <p class="control">
+          <input class="input" type="text" placeholder="Airbus 737" v-model="plane.model">
+        </p>
+        <label class="subtitle is-5">Number of seats</label>
+        <p class="control has-icon has-icon-right">
+          <input class="input" type="number" placeholder="100" v-model.number="plane.seat_count">
+        </p>
+        <p class="control">
+          <button class="button is-primary" @click="addNewPlane">Submit</button>
+          <button class="button is-link" @click="isAddShow = !isAddShow">Cancel</button>
+        </p>
     </div>
-    
   </section>
 </template>
 
@@ -19,10 +29,22 @@
     },
     data() {
       return {
-        planes: []
+        plane: {model: '', seat_count: 0},
       }
     },
     methods: {
+      addNewPlane() {
+        this.axios.post('http://localhost:80/airlines/api/planeAPI.php', {
+          model: this.plane.model,
+          seat_count: this.plane.seat_count
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      }
     },
     created() {
     },
@@ -35,6 +57,9 @@
 </script>
 
 <style scoped lang="scss">
+  .new-plane-form {
+    width: 30%;
+  }
   .plane-add {
 
     
